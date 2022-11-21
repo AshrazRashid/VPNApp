e82f1c7b798d6f384df68c743c0db792e15f5c30
ashraz <ashraz.developer@gmail.com>
2022-11-21 19:41:31 +0500

    set prettier and app js file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-
-import {store} from './src/store';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {store} from './store';
 import RootNavigator from './navigation/index';
-
 import {Provider} from 'react-redux';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
 
 const App = () => {
   return (
