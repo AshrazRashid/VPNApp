4676d8f79d02d9ac3456994b2d3aee7e2fd0fecc
ashrazdev <115641584+ashrazdev@users.noreply.github.com>
2022-11-24 11:36:34 +0500

    add library

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,33 +1,37 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import CircularProgress from 'react-native-circular-progress-indicator';
 
+let val = 0;
 const HomeScreen = () => {
-  const [counter, setCounter] = React.useState(0);
+  const [counter, setCounter] = React.useState(3);
 
   React.useEffect(() => {
     loadProgress();
   });
 
   const loadProgress = () => {
-    let val = 0;
     setTimeout(() => {
-      setCounter(val++);
+      let counterValue = counterValue--;
+      setCounter(counterValue);
     }, 3000);
   };
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <CircularProgress
-        value={60}
+        value={counter}
         radius={120}
         duration={2000}
         progressValueColor={'#ecf0f1'}
-        maxValue={200}
+        maxValue={3}
         title={'KM/H'}
         titleColor={'white'}
-        titleStyle={{fontWeight: 'bold'}}
-      />
+        titleStyle={{fontWeight: 'bold'}}>
+        <Pressable>
+          <Text>{'Connect'}</Text>
+        </Pressable>
+      </CircularProgress>
     </View>
   );
 };
